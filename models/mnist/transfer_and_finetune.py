becbb53be771bbea8d6479788a4579f23ae064e8
rana <ilredirana@gmail.com>
2018-03-29 22:47:16 +0800

    optimize finetune

--- models/mnist/transfer_and_finetune.py
+++ models/mnist/transfer_and_finetune.py
@@ -9,7 +9,6 @@ from models.mnist.dataset_reader import dataset_input_fn
 def cnn_model_no_top(features, trainable):
     """
     :param features: 原始输入
-    :param mode: estimator模式
     :param trainable: 该层的变量是否可训练
     :return: 不含最上层全连接层的模型
     """
@@ -59,20 +58,12 @@ def cnn_model_fn(features, labels, mode, params):
     loss = tf.losses.softmax_cross_entropy(onehot_labels=labels, logits=logits)
     if mode == tf.estimator.ModeKeys.TRAIN:
         # 加载已有的存档点参数的方法
-        if params.get("checkpoints") and isinstance(params.get("checkpoints"), (tuple, list)):
-            for ckpt in params.get("checkpoints"):
-                # [0]是存档点路径，[1]为是否加载倒数第二个全连接层参数
-                if ckpt[1]:
-                    print("restoring base ckpt")
-                    variables_to_restore = tf.contrib.slim.get_variables_to_restore(exclude=[logits_name])
-                    tf.train.init_from_checkpoint(ckpt[0], {v.name.split(':')[0]: v for v in variables_to_restore})
-                    print("restored base ckpt")
-                else:
-                    print("restoring transferred ckpt")
-                    variables_to_restore = tf.contrib.slim.get_variables_to_restore(exclude=[logits_name,
-                                                                                             "finetune", ])
-                    tf.train.init_from_checkpoint(ckpt[0], {v.name.split(':')[0]: v for v in variables_to_restore})
-                    print("restored transferred ckpt")
+        if (params.get("transfer") or params.get("finetune")) and not params.get("checkpoint"):
+            raise ValueError("must specify a checkpoint when transfer learning")
+        if params.get("transfer") or params.get("finetune"):
+            exclude = [logits_name] if params.get("transfer") else None
+            variables_to_restore = tf.contrib.slim.get_variables_to_restore(exclude=exclude)
+            tf.train.init_from_checkpoint(params.get("checkpoint"), {v.name.split(':')[0]: v for v in variables_to_restore})
 
         global_step = tf.train.get_or_create_global_step()
         optimizer = tf.train.AdamOptimizer(learning_rate=params.get("learning_rate", 0.0001))
@@ -124,9 +115,7 @@ if __name__ == '__main__':
     mnist_classifier = tf.estimator.Estimator(model_fn=cnn_model_fn, model_dir="./mnist_transfer_model", params={
         "transfer": True,
         "nb_classes": 2,
-        "checkpoints": [
-            (os.path.join(MODELS_DIR, "models", "mnist", "mnist_model"), True),  # (dir, load_dense_layer)
-        ]
+        "checkpoints": os.path.join(MODELS_DIR, "models", "mnist", "mnist_model"),
     })
 
     train_input_fn = dataset_input_fn(
@@ -161,9 +150,7 @@ if __name__ == '__main__':
     mnist_classifier = tf.estimator.Estimator(model_fn=cnn_model_fn, model_dir="./mnist_finetune_model", params={
         "finetune": True,
         "nb_classes": 2,
-        "checkpoints": [(os.path.join(MODELS_DIR, "models", "mnist", "mnist_model"), True),  # (dir, load_dense_layer)
-                        (os.path.join(MODELS_DIR, "models", "mnist", "mnist_transfer_model"), False)
-                        ]
+        "checkpoints": os.path.join(MODELS_DIR, "models", "mnist", "mnist_transfer_model"),
     })
 
     train_input_fn = dataset_input_fn(
